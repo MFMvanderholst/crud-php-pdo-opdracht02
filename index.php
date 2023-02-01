523bfed6bf7a359dc6ce0e37621af49cbf08f622
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2023-02-01 08:52:27 +0100

    Het eerste beginsel staat klaar

--- index.php
+++ index.php
@@ -7,6 +7,31 @@
     <title>maakzelfjepizza</title>
 </head>
 <body>
+
+<h1>Maak je eigen pizza</h1>
+
+<label for="Bodemformaat">
+    Bodemformaat
+</label>
+<select name="Bodemformaat" id="Bodemformaat" form="Bodemform">
+    <option value="#">Maak een keuze</option>
+    <option value="#">20</option>
+    <option value="#">25</option>
+    <option value="#">30</option>
+    <option value="#">35</option>
+    <option value="#">40</option>
+
+    <label for="Saus">
+    Saus
+</label>
+<select name="Saus" id="Saus" form="Sausform">
+    <option value="#">Maak een keuze</option>
+    <option value="#">Tomatensaus</option>
+    <option value="#">Extra Tomatensaus</option>
+    <option value="#">Spicy Tomatensaus</option>
+    <option value="#">BBQ Saus</option>
+    <option value="#">Créme Fraiche</option>
+</select>
     
 </body>
 </html>
